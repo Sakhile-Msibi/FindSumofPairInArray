df43b0335936b90d1ce15bf566c81d04f4ee5484
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-10-25 18:05:12 +0200

    Add project files.

---
+++ FindSumofPairInArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace FindSumofPairInArray
+{
+    class Program
+    {
+        public bool findSumofPair(int[] arr, int sum)
+        {
+            int leftIndex = Array.IndexOf(arr, arr.Min());
+            int rightIndex = Array.IndexOf(arr, arr.Max());
+
+            while(leftIndex != rightIndex)
+            {
+                if (arr[leftIndex] + arr[rightIndex] > sum)
+                    rightIndex = (arr.Length + rightIndex - 1) % arr.Length;// (N + r - 1) % N
+                else if (arr[leftIndex] + arr[rightIndex] < sum)
+                    leftIndex = (leftIndex + 1) % arr.Length;//(l+1) % N
+                else if (arr[leftIndex] + arr[rightIndex] == sum)
+                    return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            int[] arr = { 11, 15, 6, 6, 8, 9, 10 };
+            int sum = 16;
+            bool ans = program.findSumofPair(arr, sum);
+
+            Console.WriteLine(ans);
+        }
+    }
+}
